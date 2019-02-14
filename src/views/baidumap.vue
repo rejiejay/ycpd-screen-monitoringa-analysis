343975b516cce22cc4dd85184dbbf384a5f233ca
rejiejay <454766952@qq.com>
2019-02-14 11:03:04 +0800

    update

--- src/views/baidumap.vue
+++ src/views/baidumap.vue
@@ -1,5 +1,7 @@
 <template>
-<div class="baidumap">
+<div class="baidumap" :style="`width: ${clientWidth}px; height: ${clientHeight}px;`">
+    <div id="BaiduMap" :style="`width: ${clientWidth}px; height: ${clientHeight}px;`">
+    </div>
 </div>
 </template>
 
@@ -14,7 +16,10 @@ export default {
             clientHeight: document.body.offsetHeight || document.documentElement.clientHeight || window.innerHeight, // 设备高度
         } 
     },
-	mounted: function mounted() { },
+
+	mounted: function mounted() {
+        this.initBaiduMap();
+    },
 
     methods: {
         /**
@@ -22,15 +27,27 @@ export default {
          */
         initBaiduMap: function initBaiduMap() {
             this.mountBaiduMap = new BMap.Map('BaiduMap'); // 创建地图实例  
+            
             this.mountBaiduMap.centerAndZoom(new BMap.Point(114.059560, 22.542860), 13); // 初始化地图，设置中心点坐标(深圳福田) 和地图级别  
             this.mountBaiduMap.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
             this.mountBaiduMap.addControl(new BMap.NavigationControl({
                 offset: new BMap.Size(10, 65),
             }));
+            
+			this.mountBaiduMap.setMapStyle({ 
+                features: ["road", "water", "land"], // point（兴趣点）、road（道路）、water（河流）、land（陆地）、building（建筑物）
+                style : "dark"  //设置地图风格为高端黑
+            });
 
-            // this.renderMarkerPoint();
+            this.renderMarkerPoint();
         },
 
+        /**
+         * 渲染热力图标记点
+         */
+        renderMarkerPoint: function renderMarkerPoint() {
+
+        },
     }
 }
 
@@ -46,7 +63,7 @@ export default {
     position: relative;
     font-size: 14px;
     width: 100%;
-    min-height: 100%;
+    height: 100%;
 }
 
 </style>
