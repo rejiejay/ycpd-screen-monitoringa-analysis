2aaf523ea77ab373d034a1269c89c64ead54c7ec
rejiejay <454766952@qq.com>
2019-02-15 17:33:28 +0800

    add initDotService

--- src/views/baidumap.vue
+++ src/views/baidumap.vue
@@ -28,6 +28,11 @@ export default {
             mountBaiduMap: new BMap.Map('BaiduMap'),
 
             /**
+             * 正在执行标注的动画的数量
+             */
+            mrkerAnimationingCount: 0,
+
+            /**
              * 所有网点
              */
             heatmapList: [
@@ -36,6 +41,17 @@ export default {
                 //     lat: 22.667178,
                 //     count: 100,
                 // },
+            ],
+
+            /**
+             * 网点服务动态数据
+             */
+            dotServiceList: [
+                // {
+                //     whattime: "2019-02-15T14:00:11.997",
+                //     lng: 113.249232,
+                //     lat: 22.850989,
+                // }
             ],
         } 
     },
@@ -115,6 +131,48 @@ export default {
          * 初始化网点服务动态数据
          */
         initDotService: function initDotService() {
+            const _this = this;
+            
+            /**
+             * 是否 正在处理网点服务的动态数据
+             * 初始化为 未执行
+             */
+            let isHandleServiceList = false;
+
+            /**
+             * 处理网点服务动态数据的方法
+             */
+            function serviceListHandle() {
+                /**
+                 * 判断是否正在执行，用于保证仅仅执行一次
+                 */
+                if (isHandleServiceList) {
+                    // 如果存在正在执行的情况, 禁止继续执行
+                    return false
+                }
+
+                isHandleServiceList = true; // 设置为正在处理网点服务的动态数据，防止重复执行
+
+                // 当前时间戳
+                let nowTimestamp = new Date().getTime() - (1000 * 60 * 10); // 减去十分钟，因为数据是延迟，不是实时的
+                let nextTimestamp = nowTimestamp + 1000; // 下一秒
+
+                _this.dotServiceList.map(val => {
+                    /**
+                     * 判断 网点服务的时间 是否在当前的时间
+                     */
+                    if (nowTimestamp < val.timestamp && nowTimestamp > val.timestamp ) {
+                        // 如果是在当前的时间, 则将提示动画渲染到地图上
+                        _this.renderMarkerAnimation(val.lng, val.lat);
+                    }
+                });
+
+                // 一秒钟执行一次
+                setTimeout(() => {
+                    isHandleServiceList = false; // 设置为处理完毕，否则不会继续执行
+                    serviceListHandle();
+                }, 1000);
+            }
             
             /**
              * 获取网点服务动态数据
@@ -124,25 +182,33 @@ export default {
                 let value = response.data;
 
                 if (value.Code === 200 && !value.Msg) {
-                    console.log(value);
+                    console.log('获取网点服务动态数据', value);
+                    _this.dotServiceList = value.Data.details.map(val => ({
+                        timestamp: new Date(val.whattime).getTime(),
+                        lng: val.lng,
+                        lat: val.lat,
+                    }));
+
+                    serviceListHandle(); // 处理网点服务动态数据的方法
+
                 } else {
                     console.error(value);
-                    alert(`获取网点服务动态数据, 原因: ${value.Msg}`);
+                    alert(`获取网点服务动态数据失败, 原因: ${value.Msg}`);
                 }
 
             }).catch(error => {
                 console.error(error);
-                alert(`获取网点服务动态数据, 原因: ${error.message}`);
+                alert(`获取网点服务动态数据失败, 原因: ${error.message}`);
             });
-
-            this.renderMarkerAnimation(); // 渲染新增提示的动画
         },
 
         /**
          * 渲染新增提示的动画
          */
-        renderMarkerAnimation: function renderMarkerAnimation() {
+        renderMarkerAnimation: function renderMarkerAnimation(longitude, latitude) {
             const _this = this;
+
+            _this.mrkerAnimationingCount++; // 每执行一次 正在执行标注的动画的数量 加一
 
             function ComplexCustomOverlay(point , marker) {  
                 this._point = point;  
@@ -198,27 +264,38 @@ export default {
                 return plex;
             }
 
-            var myAddMarker = addMarker(114.059560, 22.542860);  
+            var myAddMarker = addMarker(longitude, latitude);  
             this.mountBaiduMap.addOverlay(myAddMarker);  
 
             /**
              * 清除动画的方法
              */
             setTimeout(() => {
-                var allOverlay = _this.mountBaiduMap.getOverlays();
+                /**
+                 * 3秒表示执行完毕
+                 * 正在执行标注的动画的数量 减一
+                 */
+                _this.mrkerAnimationingCount--;
 
-                allOverlay.map(val => {
-                    /**
-                     * 判断是否热力图
-                     */
-                    if (!val.heatmap) {
+                // 判断是否所有 动画是否执行完毕
+                if (_this.mrkerAnimationingCount <= 0) {
+                    // 所有动画执行完毕的情况下 清空动画标注
+                    var allOverlay = _this.mountBaiduMap.getOverlays();
+
+                    allOverlay.map(val => {
                         /**
-                         * 清除所有除热力图的遮罩物
-                         * 也就是动画
+                         * 判断是否热力图
                          */
-                        _this.mountBaiduMap.removeOverlay(val);
-                    }
-                });
+                        if (!val.heatmap) {
+                            /**
+                             * 清除所有除热力图的遮罩物
+                             * 也就是所有动画标注
+                             */
+                            _this.mountBaiduMap.removeOverlay(val);
+                        }
+                    });
+                }
+
             }, 3000);
 
         },
